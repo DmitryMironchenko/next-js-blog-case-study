6ed271b1a6b1cdc159f10ddf5843abb6866635b8
Dmitry Mironchenko <d.mironchenko@gmail.com>
2023-09-05 00:01:24 +0200

    Fix dark mode colors

--- app/blog-posts/post.tsx
+++ app/blog-posts/post.tsx
@@ -9,7 +9,7 @@ const Post: FC<{ post: Post }> = ({ post }) => {
       <div className="flex gap-x-2 h-32 bg-cover" style={{ backgroundImage: `url(${post.imageUrl})` }} />
       <div className="flex flex-col flex-1 px-4">
         <p className="text-xs text-cyan-700/75 my-2">{post.categories.map((category) => category.name).join(' ')}</p>
-        <h2 className="font-semibold mb-3">{post.title}</h2>
+        <h2 className="font-semibold mb-3 dark:text-black">{post.title}</h2>
         <p className="flex-1 text-sm text-gray-800/50 mb-4">{post.excerpt}</p>
         <div className="flex flex-row items-center mb-4">
           <div
@@ -17,7 +17,7 @@ const Post: FC<{ post: Post }> = ({ post }) => {
             style={{ backgroundImage: 'url(https://i.pinimg.com/564x/47/75/b0/4775b0b631bb0c5662860d3febdbab94.jpg' }}
           />
           <div className="flex flex-col ml-2 gap-1">
-            <h3 className="text-xs font-semibold">Buster Scruggs</h3>
+            <h3 className="text-xs font-semibold dark:text-black">Buster Scruggs</h3>
             <p className="text-xs text-gray-800/50">Jan 1, 2021 • 6 min read</p>
           </div>
         </div>
